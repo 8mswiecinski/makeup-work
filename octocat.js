8e6043169fcc2d888281ffcb979e625606a4e19a
8mswiecinski <8mswiecinski@stpatrick.org>
2016-12-16 10:52:56 -0600

    day 1

---
+++ octocat.js
@@ -0,0 +1,14 @@
+function octocat(n,a){
+  this.name = n;
+  this.arms = a;
+
+
+  this.slap = function(){
+  for(var a =0; a<this.arms;a++){
+    console.log("SLAP")"
+;
+};
+]
+
+
+var octal = new octocat("sam",10)
